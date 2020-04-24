32f8beb46f32044c05ca5e4ab4723a0206bb7529
rocky9413 <liangwen511@gmail.com>
2020-04-24 16:10:44 -0400

    code-splitting

--- src/svg/svgLists.js
+++ src/svg/svgLists.js
@@ -8,7 +8,6 @@ const SvgLists = () => {
       <h3>
         <a href="/">Back to Main Page</a>
       </h3>
-      <h3>adding new text</h3>
       <ul>
         {SvgRoutes.map(({ path, name }) => (
           <li key={path}>
@@ -17,17 +16,13 @@ const SvgLists = () => {
             </Link>
           </li>
         ))}
-        {/* <li><Link to="/svg/barchart">Bar Chart</Link></li>  */}
       </ul>
 
       <hr />
 
       {/* {SvgRoutes.filter(({ path }) => path !== '/svg').map(
         ({ path, exact, component: Component, ...rest }) => (
-          <Route
-            key={path}
-            path={path}
-            exact={exact}
+          <Route key={path} path={path} exact={exact}
             render={props => <Component {...props} {...rest} />}
           />
         )
@@ -42,3 +37,54 @@ const SvgLists = () => {
 };
 
 export default SvgLists;
+
+// =================================================
+// Allow components to be controlled elsewhere
+
+// import React from 'react'
+// import './styles.css'
+
+// function Navbar({
+//   items,
+//   selectedItem: selectedItemProp,
+//   onSelect: onSelectProp = () => {},
+// }) {
+//   const { current: isControlled } = React.useRef(selectedItemProp !== undefined)
+//   const [selectedItem, setSelectedItem] = React.useState(
+//     isControlled ? selectedItemProp : '/home',
+//   )
+
+//   function onClick(value) {
+//     return (e) => {
+//       if (isControlled) onSelectProp(value, e)
+//       else setSelectedItem(value)
+//     }
+//   }
+
+//   const selectedItemResult = isControlled ? selectedItemProp : selectedItem
+
+//   return (
+//     <ul>
+//       {items.map((item) => (
+//         <li key={item.to} onClick={onClick(item.to)}
+//           style={{listStyle: 'none', display: 'inline-block', marginRight: 15,
+//             color: selectedItemResult === item.to ? 'purple' : undefined }}
+//         > {item.label} </li>
+//       ))}
+//     </ul>
+//   )
+// }
+
+// function App() {
+//   const [selectedItem, setSelectedItem] = React.useState('/home')
+//   const items = [
+//     { to: '/home', label: 'Home' }, { to: '/blog', label: 'Blog' },
+//     { to: '/about', label: 'About' }, { to: '/contact', label: 'Contact' } ]
+
+//   return (
+//     <div>
+//       <Navbar items={items} selectedItem={selectedItem} onSelect={setSelectedItem} />
+//     </div>
+//   )
+// }
+// export default App
